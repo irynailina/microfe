2089fb15abac053d5d381764971499eb1f682a85
irynailina <irynailina@gmail.com>
2021-04-26 20:54:15 +0300

    finished feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "../components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>Hello there guys and girls!!!</h1>
+        <h1>Hello there guys and girls!!! This is a feature!</h1>
         <hr />
         <MarketingApp />
     </div>;
